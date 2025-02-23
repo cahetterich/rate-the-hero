2ee408901c8311baa5b6178d65f5b3446a2b4d8c
Carla H <hetterichcarla@gmail.com>
2025-02-23 19:48:16 +0100

    flex

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,18 +1,60 @@
+import { Flex, Box } from 'reflexbox';
+import styled from 'styled-components';
 import { Button } from '../common-components/Button/Button';
-import { ButtonLink } from '../common-components/ButtonLink/ButtonLink';
-import { Caption } from '../common-components/Caption/Caption';
-import { Card } from '../common-components/Card/Card';
-import { Description } from '../common-components/Description/Description';
-import { Header } from '../common-components/Header/Header';
-import { HeadingOne } from '../common-components/HeadingOne/HeadingOne';
-import { HeadingTwo } from '../common-components/HeadingTwo/HeadingTwo';
 import { SearchField } from '../common-components/SearchField/SearchField';
-import {
-	SelectField,
-	Option,
-} from '../common-components/SelectField/SelectField';
-import { Subtitle } from '../common-components/Subtitle/Subtitle';
-
+import { HeroCard } from '../components/HeroCard/HeroCard';
+import { Spaces } from '../shared/DesignTokens';
+const HeroesGrid = styled(Box)`
+	display: grid;
+	grid-template-columns: 1fr;
+	gap: ${Spaces.ONE_HALF};
+	@media (min-width: 1024px) {
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		gap: ${Spaces.TWO};
+	}
+`;
 export function Search() {
-	return <div />;
+	return (
+		<>
+			<Flex
+				width={['100%', '600px']}
+				mx={[Spaces.None, 'auto']}
+				mt={[Spaces.THREE, Spaces.FIVE]}
+				px={[Spaces.ONE, Spaces.NONE]}
+				mb={[Spaces.TWO, Spaces.FOUR]}
+			>
+				<Box flexGrow="1">
+					<SearchField placeholder="Digite um nome de herói ou heroína" />
+				</Box>
+				<Box ml={Spaces.TWO}>
+					<Button>Buscar</Button>
+				</Box>
+			</Flex>
+			
+			<HeroesGrid
+				px={[Spaces.ONE, Spaces.TWO]}
+				pb={[Spaces.ONE, Spaces.TWO]}
+			>
+				<HeroCard
+					secretIdentity="Terry McGinnis"
+					name="Batman"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/10441.jpg"
+					universe="DC Comics"
+				/>
+				<HeroCard
+					secretIdentity="Bruce Wayne"
+					name="Batman"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+					universe="DC Comics"
+				/>
+				<HeroCard
+					secretIdentity="Dick Grayson"
+					name="Batman II"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"
+					universe="DC Comics"
+				/>
+			</HeroesGrid>
+
+		</>
+	);
 }
