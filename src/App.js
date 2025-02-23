5989567641a924d0264253d1484a313018cbecc1
Carla H <hetterichcarla@gmail.com>
2025-02-23 19:31:10 +0100

    img icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Search } from "./screens/Search";
-import { Details } from "./screens/Details";
-import { NotFound } from "./screens/NotFound";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Search } from './screens/Search';
+import { Details } from './screens/Details';
 import { NormalizeStyles } from './shared/NormalizeStyles';
-
+import { Header } from './common-components/Header/Header';
 export function App() {
 	return (
 		<>
 			<NormalizeStyles />
 			<BrowserRouter>
+				<Header />
 				<Switch>
 					<Route path="/detalhes/:id" exact>
 						<Details />
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -12,27 +12,7 @@ import {
 	Option,
 } from '../common-components/SelectField/SelectField';
 import { Subtitle } from '../common-components/Subtitle/Subtitle';
+
 export function Search() {
-	return (
-		<div>
-			<Header />
-			<SearchField placeholder="Digite um nome de herói ou heroína" />
-			<Button ghost={true}>Buscar</Button>
-			<Card>
-				<HeadingOne>Heading One</HeadingOne>
-				<HeadingTwo>Heading Two</HeadingTwo>
-				<div>
-					<Subtitle>Subtitle</Subtitle>
-				</div>
-				<Description>Description</Description>
-				<div>
-					<Caption>Caption</Caption>
-				</div>
-			</Card>
-			<ButtonLink>Ver Mais</ButtonLink>
-			<SelectField placeholder="Nota">
-				<Option>Opção 1</Option>
-			</SelectField>
-		</div>
-	);
+	return <div />;
 }
